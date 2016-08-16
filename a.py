6bc19061804329a3789b1fde0e0a358e0012cb49
Ben Laor <ben@traxretail.com>
2016-08-16 15:43:44 +0300

    made a.py

--- a.py
+++ a.py
@@ -1,7 +1,11 @@
 try:
 	action = argv[1]
 	branch = argv[2]
-	if action.split()[0]=="merge" or branch not in action.split()[1:]:
+	print "action", action.split()
+	print "branch", branch
+	if action.split()[0]=="merge":
+		exit(1)
+	if action.split()[1:] and branch not in action.split()[1:]:
 		exit(1)
 	exit(0)
 except:
